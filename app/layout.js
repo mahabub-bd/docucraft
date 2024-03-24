ce850d96a4c477544c11e7350a4d317b78157cab
mahabub-bd <palashmahabub@gmail.com>
2024-03-24 07:36:40 +0600

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,18 @@
+import { getDocuments } from "@/lib/doc";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Docu Craft",
+  title: "DocuCraft | A Documentation Website by Protocol",
   description: "Docu Craft Learn With Sumit",
 };
 
 export default function RootLayout({ children }) {
+  const allDocuments = getDocuments();
+  console.log(allDocuments);
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
--- app/page.js
+++ app/page.js
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div>Docu Craft</div>;
+  return <div className="text-center">Docu Craft</div>;
 }
